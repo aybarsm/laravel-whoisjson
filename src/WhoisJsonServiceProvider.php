23e8a6cd6808a326094a59709c26dde5d385c3d0
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2026-08-12 18:22:59 +0300

    barebone

---
+++ src/WhoisJsonServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aybarsm\Laravel\WhoisJson;
+
+use Illuminate\Support\ServiceProvider;
+
+class WhoisJsonServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
